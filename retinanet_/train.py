8783e5be42ff4e0f4706d791074fcd5d1314be31
junghye01 <junghyekim01@gmail.com>
2023-12-02 16:18:16 +0000

    update retinanet code

--- retinanet_/train.py
+++ retinanet_/train.py
@@ -16,7 +16,7 @@ from torchvision.utils import make_grid
 from torch.utils.data import DataLoader, Dataset
 
 from retinanet import model
-from retinanet.dataloader import collater, Resizer, Augmenter, Normalizer, UnNormalizer,CSVDataset
+from retinanet.dataloader import collater, Resizer, Augmenter, Normalizer, UnNormalizer,CSVDataset,AspectRatioBasedSampler
 from config import Config
 from dataset import RT_Dataset
 import wandb
@@ -76,7 +76,7 @@ def train_one_epoch(retinanet,epoch_num,optimizer,scheduler, train_data_loader,d
     })
     # Update the learning rate
     if lr_scheduler is not None:
-        lr_scheduler.step()
+        lr_scheduler.step(np.mean(epoch_loss))
         
     et = time.time()
     print("\n Total Time - {}\n".format(int(et - st)))
@@ -137,33 +137,39 @@ def valid_one_epoch(retinanet,epoch_num, valid_data_loader,device):
      
 if __name__=='__main__':
     # wandb project
-    wandb.init(project='capstone',name='retinanet',reinit=True)
+    wandb.init(project='capstone',name='retinanet_1123',reinit=True)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     torch.cuda.empty_cache()
     
     # dataset 
-    train_df=pd.read_csv('../../data/annotations/train_total.csv')
-    valid_df=pd.read_csv('../../data/annotations/valid_total.csv')
-    image_dir='../../data_contrast/after/Image'
+    #train_df=pd.read_csv('/home/irteam/junghye-dcloud-dir/pathfinder/pathfinder_ai/annotations_v2/train_total.csv')
+    #valid_df=pd.read_csv('/home/irteam/junghye-dcloud-dir/pathfinder/pathfinder_ai/annotations_v2/valid_total.csv')
+    #image_dir='/home/irteam/junghye-dcloud-dir/pathfinder/data_contrast/before/Image'
 
-    train_dataset=RT_Dataset(train_df,image_dir,transforms=T.Compose([Augmenter(),Normalizer(),Resizer()]))
-    valid_dataset=RT_Dataset(valid_df,image_dir,transforms=T.Compose([Normalizer(),Resizer()]))
+    #train_dataset=RT_Dataset(train_df,image_dir,transforms=T.Compose([Augmenter(),Normalizer(),Resizer()]))
+    #valid_dataset=RT_Dataset(valid_df,image_dir,transforms=T.Compose([Normalizer(),Resizer()]))
 
+    # csv dataset
+    
+    train_dataset=CSVDataset('./annotations_v2/retinanet_train.csv','./annotations_v2/classes.csv',transform=T.Compose([Augmenter(),Normalizer()]))
+    valid_dataset=CSVDataset('./annotations_v2/retinanet_valid.csv','./annotations_v2/classes.csv',transform=T.Compose([Normalizer()]))
             
+    sampler=AspectRatioBasedSampler(train_dataset,batch_size=Config['TRAIN_BS'],drop_last=False)
+    sampler_val=AspectRatioBasedSampler(valid_dataset,batch_size=Config['VALID_BS'],drop_last=False)
+    
+    
     train_data_loader = DataLoader(
         train_dataset,
-        batch_size = Config['TRAIN_BS'],
-        shuffle = True,
         num_workers = Config['NUM_WORKERS'],
-        collate_fn = collater
+        collate_fn = collater,
+        batch_sampler=sampler,
     )
 
     valid_data_loader = DataLoader(
         valid_dataset,
-        batch_size = Config['VALID_BS'],
-        shuffle = True,
         num_workers = Config['NUM_WORKERS'],
-        collate_fn = collater
+        collate_fn = collater,
+        batch_sampler=sampler_val,
     )
     # load model
     retinanet = model.resnet50(num_classes = Config['NUM_CLASSES'], pretrained = True)
@@ -171,9 +177,11 @@ if __name__=='__main__':
     wandb.watch(retinanet)
     
     
-    optimizer = torch.optim.Adam(retinanet.parameters(), lr = Config['LR'],weight_decay=Config['WEIGHT_DECAY'])
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 5, gamma=0.5)
-
+    #optimizer = torch.optim.Adam(retinanet.parameters(), lr = Config['LR'],weight_decay=Config['WEIGHT_DECAY'])
+    optimizer=torch.optim.Adam(retinanet.parameters(),lr=Config['LR'])
+    
+    #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 5, gamma=0.5)
+    lr_scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,patience=3,verbose=True)
 
     ### Training Loop
     for epoch in range(Config['EPOCHS']):
