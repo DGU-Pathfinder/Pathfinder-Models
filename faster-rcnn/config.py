42286300c48481e6e9fb6684ee0afaae3bad0b3f
junghye01 <tweetiijh@naver.com>
2023-12-20 20:43:23 +0000

    modify faster rcnn code

--- faster-rcnn/config.py
+++ faster-rcnn/config.py
@@ -1,13 +1,13 @@
 Config=dict(
-    EPOCHS=150,
-    LR=2e-4,
-    IMG_SIZE=224,
+    EPOCHS=50,
+    LR=5e-4,
+    IMG_SIZE=640,
     DR_RATE=0.35,
     NUM_CLASSES=4,
-    TRAIN_BS=4,
-    VALID_BS=2,
+    TRAIN_BS=8,
+    VALID_BS=4,
     NUM_WORKERS=4,
     WEIGHT_DECAY=0.0005,
-    CONTRAST='AFTER'
+    CONTRAST='AFTER',
     
 )
